6cca1588d6603cad39de15b64466c7a6975c0bfd
Fayzzi <faizancuiwah@gmail.com>
2024-03-13 11:16:05 +0500

    created userModel

---
+++ backend/Models/UserModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+    select: false,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female"],
+  },
+  avater: {
+    type: String,
+    default: "",
+  },
+});
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+UserSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWTSECRETE, {
+    expiresIn: process.env.JWTEXPIRES,
+  });
+};
+UserSchema.methods.COmparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+module.exports = mongoose.model("chat-user", UserSchema);
