1b03d1eace41956ffdafbd56c460ac740a3a2bf8
Fayzzi <faizancuiwah@gmail.com>
2024-03-13 11:32:34 +0500

    signout endpoint

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -9,6 +9,17 @@ const getUser = CatchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler(error, 500));
   }
 });
+const logout = (req, res, next) => {
+  try {
+    res.cookie("userToken", "", { maxAge: 0 });
+    res.status(200).json({
+      success: true,
+      message: "User logged out",
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error, 500));
+  }
+};
 module.exports = {
   getUser,
 };
--- backend/utils/JwtToken.js
+++ backend/utils/JwtToken.js
@@ -4,6 +4,8 @@ const SendToken = (user, statusCode, res) => {
     .cookie("user_token", token, {
       httpOnly: true,
       maxAge: 15 * 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
     })
     .json({
       success: true,
